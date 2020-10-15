49276d6eed02a5a45ffa4a7ea891e1167e0b45d3
Ivanio Martins <ivaniomartins1991@gmail.com>
2020-10-15 19:17:51 -0300

    User entity and resource

---
+++ src/main/java/com/martins/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.martins.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.martins.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Ivanio", "ivaniomartins@gmail.com", "99999", "1245");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
