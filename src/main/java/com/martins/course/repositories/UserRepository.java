f1e95c59cd29a87e3d3100ffadd20b76035eabbc
Ivanio Martins <ivaniomartins1991@gmail.com>
2020-10-18 15:20:50 -0300

    Service layer, component registration

--- src/main/java/com/martins/course/repositories/UserRepository.java
+++ src/main/java/com/martins/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.martins.course.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
