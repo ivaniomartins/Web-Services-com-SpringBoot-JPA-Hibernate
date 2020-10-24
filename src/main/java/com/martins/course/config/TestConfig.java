21ece6833b4a3780e6d21ac29ef0fa2d1c2ae0fa
Ivanio Martins <ivaniomartins1991@gmail.com>
2020-10-24 20:59:06 -0300

    Payment, one-to-one association

--- src/main/java/com/martins/course/config/TestConfig.java
+++ src/main/java/com/martins/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.martins.course.entities.Category;
 import com.martins.course.entities.Order;
 import com.martins.course.entities.OrderItem;
+import com.martins.course.entities.Payment;
 import com.martins.course.entities.Product;
 import com.martins.course.entities.User;
 import com.martins.course.entities.enums.OrderStatus;
@@ -85,6 +86,12 @@ public class TestConfig implements CommandLineRunner {
 	
 	  orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	
+	  Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+	  o1.setPayment(pay1);
+	  
+	  orderRepository.save(o1);
+		
+	  
 	} 
     
     
--- src/main/java/com/martins/course/entities/Order.java
+++ src/main/java/com/martins/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -35,12 +37,13 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-    
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
-	
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
 	public Order() {
 
 	}
@@ -87,8 +90,16 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
-	public Set<OrderItem> getItems(){
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
 		return items;
 	}
 
