95e56eb4b09f71f083a170959a1b82a32d2b5ecd
Ivanio Martins <ivaniomartins1991@gmail.com>
2020-10-25 11:14:26 -0300

    Subtotal & Total methods

--- src/main/java/com/martins/course/entities/OrderItem.java
+++ src/main/java/com/martins/course/entities/OrderItem.java
@@ -75,6 +75,10 @@ public class OrderItem implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
